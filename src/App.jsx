784da9e661c2c43170e56004601506fbb99f7e02
Saaimm <saaimchashoo07@gmail.com>
2026-04-20 13:00:25 +0700

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+function App() {
+  return (
+    <div>
+      <h1>Saaim Chashoo</h1>
+      <p>Software Engineering Student</p>
+
+      <section>
+        <h2>About</h2>
+        <p>
+          I am a first-year Software Engineering student at KMITL.
+          I am interested in backend development, systems programming,
+          and AI.
+        </p>
+      </section>
+
+      <section>
+  <h2>Projects</h2>
+
+  <div>
+    <h3>Personal Library System</h3>
+    <p>Python project to store and manage ebooks.</p>
+    <a href="https://github.com/SaaimCash/Personal-Library-System" target="_blank">
+      View Project
+    </a>
+  </div>
+
+  <div>
+    <h3>Rust File Manager</h3>
+    <p>File system tool built using Rust.</p>
+    <a href="https://github.com/ifelsebetter/disk-usage-visualizer" target="_blank">
+      View Project
+    </a>
+  </div>
+</section>
+
+      <section>
+        <h2>Skills</h2>
+        <p>Python, Rust, C++, Power BI</p>
+      </section>
+
+      <section>
+  <h2>Contact</h2>
+  <p>Email: saaimchashoo07@gmail.com</p>
+  <p>
+    GitHub: <a href="https://github.com/SaaimCash" target="_blank">My GitHub</a>
+  </p>
+  <p>
+    LinkedIn: <a href="https://www.linkedin.com/in/saaim-chashoo-5598ab374/" target="_blank">My LinkedIn</a>
+  </p>
+</section>
+    </div>
+  );
+}
+
+export default App;
